11c2f6cc876ad25ff0b9e3b1df2dd711a4f67bbb
Neel Rao <neelorao@gmail.com>
2023-12-03 21:48:28 -0800

    feat: puzzle 4

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { puzzle1, puzzle2, puzzle3 } from '~/puzzles';
+import { puzzle1, puzzle2, puzzle3, puzzle4 } from '~/puzzles';
 
 async function start() {
     // await puzzle1.run();
     // await puzzle2.run();
-    await puzzle3.run({
+    // await puzzle3.run();
+    await puzzle4.run({
         example: true,
         mainProblem: true,
     });
