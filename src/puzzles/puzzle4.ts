b30995e22399d675a612352178c2c8e5c9b200bb
Neel Rao <neelorao@gmail.com>
2023-12-03 22:18:48 -0800

    feat: cached function utility

--- src/puzzles/puzzle4.ts
+++ src/puzzles/puzzle4.ts
@@ -1,3 +1,4 @@
+import { CachedFunction } from '~/util/CachedFunction';
 import { Puzzle } from './Puzzle';
 
 interface Card {
@@ -11,30 +12,25 @@ export const puzzle4 = new Puzzle<Card[]>({
         const cardLines = fileData.split('\n').filter((s) => s);
         return cardLines.map((cardData) => {
             const [, cardId = '', winnersString = '', cardNumbersString = ''] =
-                cardData.match(/Card\s*(\d*):\s*([\d ]*)\s*\|\s*([\d ]*)/) ??
+                cardData.match(/Card\s+(\d*):\s+([\d ]*)\s+\|\s+([\d ]*)/) ??
                 [];
 
-            const cardNumbers = [...cardNumbersString.matchAll(/\d*/g)].reduce<
-                number[]
-            >((acc, [match]) => {
-                if (match) {
-                    acc.push(parseInt(match, 10));
-                }
-                return acc;
-            }, []);
+            const isWinner: Record<string, boolean> = {};
+            winnersString.split(/\s+/g).forEach((n) => {
+                isWinner[n] = true;
+            });
 
-            const winnerMap: Record<string, boolean> = {};
-            [...winnersString.matchAll(/\d*/g)].forEach(([match]) => {
-                if (match) {
-                    winnerMap[match] = true;
-                }
-            }, []);
+            let nWinners = 0;
 
-            const winnersOnCard = cardNumbers.filter((n) => winnerMap[n]);
+            cardNumbersString.split(/\s+/g).forEach((n) => {
+                if (isWinner[n]) {
+                    nWinners++;
+                }
+            });
 
             return {
                 id: parseInt(cardId, 10),
-                nWinners: winnersOnCard.length,
+                nWinners,
             };
         });
     },
@@ -44,28 +40,24 @@ export const puzzle4 = new Puzzle<Card[]>({
         }, 0);
     },
     part2: (cards) => {
-        const scoreById: Record<string, number> = {};
-        function getScore({ id, nWinners }: Card) {
-            const cachedScore = scoreById[id];
-            if (cachedScore !== undefined) {
-                return cachedScore;
-            }
+        const scoreCalculator = new CachedFunction({
+            func: ({ id, nWinners }: Card) => {
+                let score = 1;
 
-            let score = 1;
-
-            for (let i = 0; i < nWinners; i++) {
-                const card = cards[id + i];
-                if (card) {
-                    score += getScore(card);
+                for (let i = 0; i < nWinners; i++) {
+                    const card = cards[id + i];
+                    if (card) {
+                        score += scoreCalculator.run(card);
+                    }
                 }
-            }
-            scoreById[id] = score;
 
-            return score;
-        }
+                return score;
+            },
+            getKey: (card) => card.id,
+        });
 
         return cards.reduce((sum, card) => {
-            const cardScore = getScore(card);
+            const cardScore = scoreCalculator.run(card);
             return sum + cardScore;
         }, 0);
     },
