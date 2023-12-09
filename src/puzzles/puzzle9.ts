c1f41dc3460de5479959124b4aaf18ec1edca3b1
Neel Rao <neelorao@gmail.com>
2023-12-08 23:43:37 -0800

    refactor: more efficient conditional

--- src/puzzles/puzzle9.ts
+++ src/puzzles/puzzle9.ts
@@ -10,40 +10,57 @@ export const puzzle9 = new Puzzle({
             .map((line) => line.split(/\s+/).map(Number));
     },
     part1: (histories) => {
-        function extrapolateForward(sequence: number[]) {
-            if (sequence.length < 2) {
-                throw new Error('Sequence must have at least 2 numbers');
-            }
-            const lastNumbers = [sequence[sequence.length - 1]!];
-            let diffs = getDiffs(sequence);
-            while (sum(diffs) !== 0) {
-                lastNumbers.push(diffs[diffs.length - 1]!);
-                diffs = getDiffs(diffs);
-            }
-
-            return sum(lastNumbers);
-        }
-
-        return sum(histories.map(extrapolateForward));
+        return sum(
+            histories.map((history) => {
+                const lastNumbers: number[] = [];
+                forEachDiffLayer(history, (diffs) => {
+                    const lastNumber = diffs[diffs.length - 1];
+                    if (lastNumber !== undefined) {
+                        lastNumbers.push(lastNumber);
+                    }
+                });
+                return sum(lastNumbers);
+            })
+        );
     },
     part2: (histories) => {
-        function extrapolateBackward(sequence: number[]) {
-            if (sequence.length < 2) {
-                throw new Error('Sequence must have at least 2 numbers');
-            }
-            const firstNumbers = [sequence[0]!];
-            let diffs = getDiffs(sequence);
-            while (sum(diffs) !== 0) {
-                firstNumbers.push(diffs[0]!);
-                diffs = getDiffs(diffs);
-            }
-
-            return firstNumbers.reverse().reduce((delta, x) => x - delta, 0);
-        }
-
-        return sum(histories.map(extrapolateBackward));
+        return sum(
+            histories.map((history) => {
+                const firstNumbers: number[] = [];
+                forEachDiffLayer(history, (diffs) => {
+                    const firstNumber = diffs[0];
+                    if (firstNumber !== undefined) {
+                        firstNumbers.push(firstNumber);
+                    }
+                });
+                return firstNumbers
+                    .reverse()
+                    .reduce((delta, x) => x - delta, 0);
+            })
+        );
     },
 });
+
+function forEachDiffLayer(
+    sequence: number[],
+    callback: (diffs: number[]) => void
+) {
+    if (sequence.length < 2) {
+        throw new Error('Sequence must have at least 2 numbers');
+    }
+
+    let previousSequence = sequence;
+    callback(previousSequence);
+
+    let diffs = getDiffs(sequence);
+
+    while (diffs.some((n) => n !== 0)) {
+        previousSequence = diffs;
+        callback(previousSequence);
+
+        diffs = getDiffs(diffs);
+    }
+}
 
 function getDiffs(sequence: number[]) {
     const diffs: number[] = [];
