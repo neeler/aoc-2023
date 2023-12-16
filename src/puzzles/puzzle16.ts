2754079c08aacb4fcfd2f7e03d57a222cfc7cadf
Neel Rao <neelorao@gmail.com>
2023-12-15 23:12:36 -0800

    refactor: some cleanup and lots of comments

--- src/puzzles/puzzle16.ts
+++ src/puzzles/puzzle16.ts
@@ -3,6 +3,69 @@ import { Point } from '~/types/Point';
 import { Queue } from '~/types/Queue';
 import { Puzzle } from './Puzzle';
 
+export const puzzle16 = new Puzzle({
+    day: 16,
+    parseInput: (fileData) =>
+        new Contraption(
+            fileData
+                .split('\n')
+                .filter((s) => s)
+                .map((s) => s.split(''))
+        ),
+    part1: (contraption) => {
+        contraption.addLight({
+            row: 0,
+            col: 0,
+            direction: 'Right',
+        });
+        return contraption.strength;
+    },
+    part2: (contraption) => {
+        let maxStrength = 0;
+
+        for (let col = 0; col < contraption.grid.width; col++) {
+            contraption.reset();
+            contraption.addLight({
+                row: 0,
+                col,
+                direction: 'Down',
+            });
+            maxStrength = Math.max(maxStrength, contraption.strength);
+
+            contraption.reset();
+            contraption.addLight({
+                row: contraption.grid.height - 1,
+                col,
+                direction: 'Up',
+            });
+            maxStrength = Math.max(maxStrength, contraption.strength);
+        }
+
+        for (let row = 0; row < contraption.grid.height; row++) {
+            contraption.reset();
+            contraption.addLight({
+                row,
+                col: 0,
+                direction: 'Right',
+            });
+            maxStrength = Math.max(maxStrength, contraption.strength);
+
+            contraption.reset();
+            contraption.addLight({
+                row,
+                col: contraption.grid.width - 1,
+                direction: 'Left',
+            });
+            maxStrength = Math.max(maxStrength, contraption.strength);
+        }
+
+        return maxStrength;
+    },
+});
+
+/**
+ * The xy-differences for each direction.
+ */
 const DirectionDiffs = {
     Up: new Point(0, -1),
     Down: new Point(0, 1),
@@ -15,7 +78,7 @@ class Tile {
     readonly position: Point;
     readonly symbol: string;
     readonly grid: Grid<Tile>;
-    readonly lights: Direction[] = [];
+    readonly lights = new Set<Direction>();
 
     constructor({
         position,
@@ -31,79 +94,25 @@ class Tile {
         this.grid = grid;
     }
 
+    /**
+     * Reset the tile to its initial state.
+     */
     reset() {
-        this.lights.length = 0;
+        this.lights.clear();
     }
 
     toString() {
         return this.symbol;
     }
 
+    /**
+     * Whether the tile is energized
+     * AKA the tile has any light traveling through.
+     */
     get isEnergized() {
-        return this.lights.length > 0;
+        return this.lights.size > 0;
     }
 }
-
-export const puzzle16 = new Puzzle({
-    day: 16,
-    parseInput: (fileData) =>
-        new Contraption(
-            fileData
-                .split('\n')
-                .filter((s) => s)
-                .map((s) => s.split(''))
-        ),
-    part1: (contraption) => {
-        contraption.reset();
-        contraption.addLight({
-            direction: 'Right',
-            row: 0,
-            col: 0,
-        });
-        return contraption.strength;
-    },
-    part2: (contraption) => {
-        const possibilities: number[] = [];
-
-        for (let col = 0; col < contraption.width; col++) {
-            contraption.reset();
-            contraption.addLight({
-                row: 0,
-                col,
-                direction: 'Down',
-            });
-            possibilities.push(contraption.strength);
-
-            contraption.reset();
-            contraption.addLight({
-                row: contraption.height - 1,
-                col,
-                direction: 'Up',
-            });
-            possibilities.push(contraption.strength);
-        }
-
-        for (let row = 0; row < contraption.height; row++) {
-            contraption.reset();
-            contraption.addLight({
-                row,
-                col: 0,
-                direction: 'Right',
-            });
-            possibilities.push(contraption.strength);
-
-            contraption.reset();
-            contraption.addLight({
-                row,
-                col: contraption.width - 1,
-                direction: 'Left',
-            });
-            possibilities.push(contraption.strength);
-        }
-
-        return Math.max(...possibilities);
-    },
-});
 
 class Contraption {
     readonly grid: Grid<Tile>;
@@ -124,14 +133,9 @@ class Contraption {
         );
     }
 
-    get width() {
-        return this.grid.width;
-    }
-
-    get height() {
-        return this.grid.height;
-    }
-
+    /**
+     * Reset the contraption to its initial state.
+     */
     reset() {
         this.queue.reset();
         this.grid.forEach((tile) => {
@@ -139,6 +143,14 @@ class Contraption {
         });
     }
 
+    /**
+     * Add a light to the contraption.
+     * @param options The options for adding the light.
+     * @param options.direction The direction the light is coming from.
+     * @param [options.row] The row of the tile to add the light to.
+     * @param [options.col] The column of the tile to add the light to.
+     * @param [options.tile] The tile to add the light to.
+     */
     addLight({
         direction,
         row: inputRow,
@@ -156,25 +168,51 @@ class Contraption {
                 ? this.grid.getAt(inputRow, inputCol)
                 : undefined);
 
+        /**
+         * Add the starting tile to the queue.
+         */
         this.queue.enqueue({ tile, direction });
+
+        /**
+         * Process the queue.
+         */
         this.queue.process(({ tile, direction }) => {
-            if (!tile || tile.lights.includes(direction)) {
+            /**
+             * If the tile is undefined
+             * or already has a light coming from this direction,
+             * skip it.
+             */
+            if (!tile || tile.lights.has(direction)) {
                 return;
             }
 
-            tile.lights.push(direction);
+            /**
+             * Add the light to the tile.
+             */
+            tile.lights.add(direction);
 
+            /**
+             * Get the directions to add to the queue,
+             * based on the tile's symbol
+             * and the direction the light is coming from.
+             */
             const directions = DirectionFlips[tile.symbol]?.[direction] ?? [
                 direction,
             ];
 
             directions.forEach((direction) => {
+                /**
+                 * Get the next tile in the given direction.
+                 */
                 const diff = DirectionDiffs[direction];
                 const nextTile = this.grid.getAt(
                     tile.position.row + diff.row,
                     tile.position.col + diff.col
                 );
                 if (nextTile) {
+                    /**
+                     * Add the tile to the queue.
+                     */
                     this.queue.enqueue({
                         tile: nextTile,
                         direction,
@@ -184,11 +222,18 @@ class Contraption {
         });
     }
 
+    /**
+     * The strength of the contraption.
+     * This is the number of tiles that are energized.
+     */
     get strength() {
         return this.grid.filter((tile) => tile.isEnergized).length;
     }
 }
 
+/**
+ * Map of symbols and incoming directions to the resulting directions.
+ */
 const DirectionFlips: Record<
     string,
     Partial<Record<Direction, Direction[]>>
