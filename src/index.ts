379afc2d99e6ba4c41a08d64f5f3d4ba5be7e809
Neel Rao <neelorao@gmail.com>
2023-12-20 01:06:25 -0800

    chore: new total run time - 6.346s

--- src/index.ts
+++ src/index.ts
@@ -26,26 +26,26 @@ import { Timer } from '~/util/Timer';
 async function start() {
     const timer = new Timer();
 
-    // await puzzle1.run();
-    // await puzzle2.run();
-    // await puzzle3.run();
-    // await puzzle4.run();
-    // await puzzle5.run();
-    // await puzzle6.run();
-    // await puzzle7.run();
-    // await puzzle8.run();
-    // await puzzle9.run();
-    // await puzzle10.run();
-    // await puzzle11.run();
-    // await puzzle12.run();
-    // await puzzle13.run();
-    // await puzzle14.run();
-    // await puzzle15.run();
-    // await puzzle16.run();
+    await puzzle1.run();
+    await puzzle2.run();
+    await puzzle3.run();
+    await puzzle4.run();
+    await puzzle5.run();
+    await puzzle6.run();
+    await puzzle7.run();
+    await puzzle8.run();
+    await puzzle9.run();
+    await puzzle10.run();
+    await puzzle11.run();
+    await puzzle12.run();
+    await puzzle13.run();
+    await puzzle14.run();
+    await puzzle15.run();
+    await puzzle16.run();
     await puzzle17.run();
-    // await puzzle18.run();
-    // await puzzle19.run();
-    // await puzzle20.run();
+    await puzzle18.run();
+    await puzzle19.run();
+    await puzzle20.run();
 
     console.log(kleur.cyan(`All puzzles ran in ${timer.time}.`));
 }
