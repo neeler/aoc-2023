148b275efee017dc5fdea0c02e15580abb9825d4
Neel Rao <neelorao@gmail.com>
2023-12-24 22:39:18 -0800

    chore: run all puzzles
    All puzzle solutions run sequentially in a total of 24s!
    Day 23 part 2 is the main culprit, with a 14-15s runtime.

--- src/index.ts
+++ src/index.ts
@@ -31,34 +31,31 @@ import { Timer } from '~/util/Timer';
 async function start() {
     const timer = new Timer();
 
-    // await puzzle1.run();
-    // await puzzle2.run();
-    // await puzzle3.run();
-    // await puzzle4.run();
-    // await puzzle5.run();
-    // await puzzle6.run();
-    // await puzzle7.run();
-    // await puzzle8.run();
-    // await puzzle9.run();
-    // await puzzle10.run();
-    // await puzzle11.run();
-    // await puzzle12.run();
-    // await puzzle13.run();
-    // await puzzle14.run();
-    // await puzzle15.run();
-    // await puzzle16.run();
-    // await puzzle17.run();
-    // await puzzle18.run();
-    // await puzzle19.run();
-    // await puzzle20.run();
-    // await puzzle21.run();
-    // await puzzle22.run();
-    // await puzzle23.run();
-    // await puzzle24.run();
-    await puzzle25.run({
-        example: true,
-        mainProblem: true,
-    });
+    await puzzle1.run();
+    await puzzle2.run();
+    await puzzle3.run();
+    await puzzle4.run();
+    await puzzle5.run();
+    await puzzle6.run();
+    await puzzle7.run();
+    await puzzle8.run();
+    await puzzle9.run();
+    await puzzle10.run();
+    await puzzle11.run();
+    await puzzle12.run();
+    await puzzle13.run();
+    await puzzle14.run();
+    await puzzle15.run();
+    await puzzle16.run();
+    await puzzle17.run();
+    await puzzle18.run();
+    await puzzle19.run();
+    await puzzle20.run();
+    await puzzle21.run();
+    await puzzle22.run();
+    await puzzle23.run();
+    await puzzle24.run();
+    await puzzle25.run();
 
     console.log(kleur.cyan(`All puzzles ran in ${timer.time}.`));
 }
